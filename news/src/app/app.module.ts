bc67c8eac45473a4a9aa8497e13efd8372f4d0d6
EvgeniyaAnisko <evgeniya.anisko@gmail.com>
2022-10-06 21:09:15 +0300

    workshop RS School

---
+++ news/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FeedComponent } from './components/feed/feed.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { SearchPipe } from './pipes/search.pipe';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { AddPostComponent } from './components/add-post/add-post.component';
+import { PostComponent } from './components/post/post.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FeedComponent,
+    SearchPipe,
+    SearchBarComponent,
+    AddPostComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    FormsModule,
+  ],
+  providers: [SearchPipe],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
